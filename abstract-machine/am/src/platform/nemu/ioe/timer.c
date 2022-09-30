a75488f1d69ff5edcefa9dc97b0c4ef3ae12ba5f
literaryno4 <m18260037123@163.com>
2022-09-30 11:43:42 +0800

    better timer uptime

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -1,19 +1,14 @@
 #include <am.h>
 #include <nemu.h>
-#include <stdio.h>
-
-static uint64_t read_time() {
-  uint32_t lo = *(volatile uint32_t *)(RTC_ADDR + 0);
-  uint32_t hi = *(volatile uint32_t *)(RTC_ADDR + 4);
-  uint64_t time = ((uint64_t)hi << 32) | lo;
-  return time;
-}
 
 void __am_timer_init() {
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = read_time();
+  uptime->us = 0;
+  uptime->us = inl(RTC_ADDR+4);
+  uptime->us <<= 32;
+  uptime->us += inl(RTC_ADDR);
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
